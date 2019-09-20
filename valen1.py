eef24de92fd04d7958b40f06c9e92b44e082a30d
Miguel <mfrasquetherraiz@gmail.com>
2019-09-20 20:28:24 +0200

    created csv to record the data

--- valen1.py
+++ valen1.py
@@ -12,8 +12,9 @@ import urllib.request
 import pandas as pd
 import numpy as np
 import json
+import datetime
 
-
+#Load my key
 with open('/home/miguel/Desktop/Python_files/valenbisi/key.txt') as f:
     key = f.readlines()
 
@@ -32,3 +33,18 @@ standsHome=home['available_bike_stands']
 office= JSON_object[117] #Data from the station near office at the Uni
 bikesOffice=office['available_bikes']
 standsOffice=office['available_bike_stands']
+
+now = datetime.datetime.now()
+bikes=pd.read_csv('bikes.csv', sep='\t',index_col=False)
+bikes.loc[len(bikes)+1] = [now,bikesHome,standsHome,bikesOffice,standsOffice]
+bikes.to_csv('bikes.csv', sep='\t', index=False)
+
+
+#columnsList=['bikesHome','standsHome','bikesOffice','standsOffice']
+#bikes = pd.DataFrame(columns=columnsList)
+
+#tatus = pd.read_csv('/home/miguel/Desktop/Python_files/PLAT_VIRT/meteo/status.csv', sep='\t')
+#status .drop(status .columns[0], axis=1)
+#results_day=pd.DataFrame({ fecha: results})
+#status=pd.concat([status,results_day], axis=1)
+#status.to_csv('/home/miguel/Desktop/Python_files/PLAT_VIRT/meteo/status.csv', sep='\t', index=False)
